26c59a982534acc6863537efb305cb9428b2b92c
Arun23592 <arunsubramani1992@gmail.com>
2023-02-23 22:13:21 +0530

    commit3

--- 01-Fundamentals-Part-1/starter/Script.js
+++ 01-Fundamentals-Part-1/starter/Script.js
@@ -200,3 +200,88 @@ console.log("23" / "2");
 let n = "1" + 1;
 n = n - 1;
 console.log(n);
+
+//Truthy and Falsy values
+// There 5 Falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean("Arun"));
+console.log(Boolean(undefined));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 100;
+if (money) {
+  console.log("Dont spent money!");
+} else {
+  console.log("You should get a Job!");
+}
+
+//Equality operator === vs == //Strict equality === recommended
+
+const yourAge = 18;
+if (yourAge === 18) console.log("you must be an adult (strict)");
+
+if (yourAge == 18) console.log("you must be an adult(loose)");
+
+const favourite = Number(prompt("What is your favourite number"));
+console.log(favourite);
+
+if (favourite === 23) {
+  console.log("Cool! that's an awesome number");
+} else if (favourite === 7) {
+  console.log("7 is also a awesome number");
+} else {
+  console.log("Number is not 23 or 7");
+}
+
+if (favourite != 23) console.log("WHy not 23?");
+
+//Boolen logic:AND OR NOT OPERATORS
+//lOGICAL operators
+
+const hasDriversLicence = true;
+const hasGoodVision = false;
+
+console.log(hasDriversLicence && hasGoodVision);
+console.log(hasDriversLicence || hasGoodVision);
+console.log(!hasDriversLicence);
+
+if (hasDriversLicence && hasGoodVision) {
+  console.log("Sarah is able to drive");
+} else {
+  console.log("SomeOne should drive");
+}
+
+const isTired = true;
+console.log(hasDriversLicence && hasGoodVision && isTired);
+
+//Coding chalalenge #3
+
+// There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins the a trophy!
+
+// 1. Calculate the average score for each team, using the test data below
+// 2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
+
+// 3. BONUS 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+// 4. BONUS 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy.
+
+// TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// TEST DATA BONUS 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+// GOOD LUCK 😀
+
+const dolphinsScore = (96 + 108 + 89) / 3;
+const koalasScore = (88 + 91 + 110) / 3;
+
+if (dolphinsScore > koalasScore && dolphinsScore >= 100) {
+  console.log("Dolphins wins the trophy🗽");
+} else if (koalasScore > dolphinsScore && koalasScore >= 100) {
+  console.log("Koalas wins the tropohy🗼");
+} else if (dolphinsScore === koalasScore) {
+  console.log("Both wins the trophy🤣");
+} else {
+  console.log("No one wins the trophy😥");
+}
+
+//////////////////////////////////////
