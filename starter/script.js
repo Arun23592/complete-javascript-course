f0fdc67abe2130ee33428c6e0cf39a6733ce3467
Arun23592 <arunsubramani1992@gmail.com>
2023-02-24 23:28:09 +0530

    commit2

---
+++ starter/script.js
@@ -0,0 +1,10 @@
+//Activating strict
+"use strict"; //to avoid accedential errors
+
+let hasDriversLicense = false;
+const passTest = true;
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive");
+
+const interface = "Audio";
+const private = 534;
