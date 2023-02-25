9cbff453566f15d8d2fdb966f687cbeb0c8354e9
Arun Subramani <61534947+Arun23592@users.noreply.github.com>
2023-02-25 15:49:49 +0530

    Delete starter directory

--- starter/script.js
+++
@@ -1,9 +0,0 @@
-// Activating Strict Mode
-let hasDriversLicense = false;
-const passTest = true;
-
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("I can drive :D");
-
-// const interface = 'Audio';
-// const private = 534;
