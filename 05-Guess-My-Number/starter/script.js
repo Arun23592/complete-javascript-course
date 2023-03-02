648ebca203d91fdc5c500f5861e8b551a2a5d712
Arun23592 <arunsubramani1992@gmail.com>
2023-03-02 23:03:27 +0530

    GuessMyNumber

---
+++ 05-Guess-My-Number/starter/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number!!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 11;
+
+console.log((document.querySelector('.guess').value = 23));
