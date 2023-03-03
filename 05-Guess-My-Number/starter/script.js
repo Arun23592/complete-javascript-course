d6629b94ec08e3fa721d552f8ad85de8ceee725c
Arun23592 <arunsubramani1992@gmail.com>
2023-03-03 23:05:16 +0530

    GuessMyNumber2

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -8,3 +8,32 @@ document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 11;
 
 console.log((document.querySelector('.guess').value = 23));
+
+///
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    console.log((document.querySelector('.message').textContent = 'No Number'));
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number ';
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+    }
+
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+});
