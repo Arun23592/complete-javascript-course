4dd39fb90b4d86375841ca251a7dadebf374496d
Arun23592 <arunsubramani1992@gmail.com>
2023-04-05 21:55:36 +0530

    string

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -306,3 +306,25 @@ console.log(question.get(question.get('correct') === answer));
 console.log([...question]);
 console.log([...question.keys()]);
 console.log([...question.values()]);
+
+//working with strirngs
+const airline = 'TAP Air Portugal';
+const plane = 'A230';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.lastIndexOf('portugal'));
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+
+console.log(airline.slice(-2));
