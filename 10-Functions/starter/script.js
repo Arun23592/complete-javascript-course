03243249fffcc71ff6db94992c52b864dccb3e8f
Arun23592 <arunsubramani1992@gmail.com>
2023-04-10 22:15:16 +0530

    function

---
+++ 10-Functions/starter/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  booking.push(booking);
+};
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
